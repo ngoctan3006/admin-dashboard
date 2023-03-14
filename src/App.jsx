35d4689fc509bce52b86092017944561c2e7178d
Tan <>
2023-03-15 00:58:04 +0700

    settings button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import React from 'react';
+import { FiSettings } from 'react-icons/fi';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-  return <h1 className="underline text-3xl">My App</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+            <TooltipComponent content="Settings" position="TopCenter">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ backgroundColor: 'blue', borderRadius: '50%' }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
