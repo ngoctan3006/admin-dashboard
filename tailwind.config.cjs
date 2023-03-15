4c88f89d34591aaab6309040f3b7825c2d039a70
Tan <>
2023-03-15 20:39:32 +0700

    home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -39,7 +39,7 @@ module.exports = {
         590: '590px',
       },
       backgroundImage: {
-        'hero-pattern': "url('/Rectangle-4389.png')",
+        'hero-pattern': "url('/bg.svg')",
       },
     },
   },
